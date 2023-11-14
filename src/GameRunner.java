322fdc8ae87a88bfc3fcbf4f3c2da85c9daa4b7a
RDas3 <das@>
2023-11-14 14:28:59 -0500

    Added clicker counter stuff

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -2,5 +2,6 @@ class GameRunner {
     public static void main(String[] args) {
 
         GameWindow frame = new GameWindow("Welcome to my game!");
+        System.out.println("Testing");
     }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -7,31 +7,55 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 
-public class GameWindow extends JFrame implements ActionListener{
+public class GameWindow extends JFrame implements ActionListener, Runnable {
 
     private JFrame j;
     private JPanel p;
     private JLabel textLabel;
+    private JLabel bottomLabel;
     private Image image;
     private int x;
     private int y;
     private int clickerCounter;
+    private Thread windowThread;
+    private long startTime;
+
 
     public GameWindow(String display) {
+        startTime = System.currentTimeMillis();
         x = 100;
         y = 100;
         int frameWidth = 500;
         int frameHeight = 500;
         j = new JFrame(display);
         textLabel = new JLabel("Counter: " + clickerCounter);
+        bottomLabel = new JLabel("Time: " + clickerCounter);
         clickerCounter = 0;
         j.add(textLabel, BorderLayout.NORTH);
+        j.add(bottomLabel, BorderLayout.SOUTH);
         j.addKeyListener(new KeyTracker());
 
         j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         j.setSize(frameWidth, frameHeight);
         j.setLocation(5, 5);
         j.setVisible(true);
+        startThread();
+
+    }
+
+    public void startThread() {
+        windowThread = new Thread(this);
+        windowThread.start();
+    }
+
+
+    public void run() {
+        while (true) {
+            long currentTime = System.currentTimeMillis();
+            long timeElapsed = (currentTime - startTime) / 1000;
+            bottomLabel.setText("Time: " + timeElapsed + "s");
+        }
+
     }
 
     public void actionPerformed(ActionEvent e) {
