b1fbb6d8dda8e7b3f44024be216232c3bed53d21
RDas3 <das@>
2024-03-20 13:26:33 -0400

    Add fox movement and mouse events

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -2,6 +2,5 @@ class GameRunner {
     public static void main(String[] args) {
 
         GameWindow frame = new GameWindow("Welcome to my game!");
-        System.out.println("Testing");
     }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -3,9 +3,16 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyAdapter;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import javax.imageio.ImageIO;
+import java.io.File;
+
 
 public class GameWindow extends JFrame implements ActionListener, Runnable {
 
@@ -13,13 +20,11 @@ public class GameWindow extends JFrame implements ActionListener, Runnable {
     private JPanel p;
     private JLabel textLabel;
     private JLabel bottomLabel;
-    private Image image;
     private int x;
     private int y;
     private int clickerCounter;
     private Thread windowThread;
     private long startTime;
-
 
     public GameWindow(String display) {
         startTime = System.currentTimeMillis();
@@ -27,10 +32,20 @@ public class GameWindow extends JFrame implements ActionListener, Runnable {
         y = 100;
         int frameWidth = 500;
         int frameHeight = 500;
+        BufferedImage imageOrange = null;
+        BufferedImage imageBlue = null;
+        try {
+            imageOrange = ImageIO.read(new File("src/orange-fox-sprite.png"));
+            imageBlue = ImageIO.read(new File("src/blue-fox-sprite.png"));
+        }
+        catch (Exception e) {}
+
+        p = new DrawPanel(imageOrange, imageBlue);
         j = new JFrame(display);
         textLabel = new JLabel("Counter: " + clickerCounter);
         bottomLabel = new JLabel("Time: " + clickerCounter);
         clickerCounter = 0;
+        j.add(p);
         j.add(textLabel, BorderLayout.NORTH);
         j.add(bottomLabel, BorderLayout.SOUTH);
         j.addKeyListener(new KeyTracker());
@@ -54,7 +69,9 @@ public class GameWindow extends JFrame implements ActionListener, Runnable {
             long currentTime = System.currentTimeMillis();
             long timeElapsed = (currentTime - startTime) / 1000;
             bottomLabel.setText("Time: " + timeElapsed + "s");
+            p.repaint();
         }
+
 
     }
 
@@ -63,13 +80,91 @@ public class GameWindow extends JFrame implements ActionListener, Runnable {
     }
 
     public class KeyTracker extends KeyAdapter{
-        @Override
         public void keyPressed(KeyEvent e) {
-            if (e.getKeyCode() == 10) {
+            if (e.getKeyCode() == 65) {
                 clickerCounter++;
                 textLabel.setText("Counter: " + clickerCounter);
             }
         }
+    }
+}
+
+class DrawPanel extends JPanel {
+
+    private BufferedImage image;
+    private BufferedImage image1;
+    private BufferedImage currentImage;
+    private Rectangle fox;
+    double x;
+    double y;
+    boolean foxOffScreen = false;
+    int foxType = 0;
+
+
+    public DrawPanel(BufferedImage image, BufferedImage image1) {
+        x = 0;
+        y = 0;
+        fox = new Rectangle((int)x, (int)y, image.getWidth(), image.getHeight());
+        this.image = image;
+        this.image1 = image1;
+        this.currentImage = image;
+        this.addMouseListener(new EventTracker());
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(currentImage, (int)x, (int)y, null);
+        g.drawRect(0, 0, 15, 15);
+        if (foxOffScreen) {
+            x = x - .3;
+            y = y - .3;
+            fox.setLocation((int)x, (int)y);
+        }
+        else {
+            x = x + .3;
+            y = y + .3;
+            fox.setLocation((int)x, (int)y);
+        }
+        if (x > 400 && y > 400) {
+            foxOffScreen = true;
+        }
+        if (x < 0) {
+            foxOffScreen = false;
+        }
+    }
+
+    class EventTracker implements MouseListener {
+        public void mousePressed(MouseEvent e) {
+            if (fox.contains(new Point(e.getX(), e.getY()))) {
+                foxType++;
+                if (foxType == 2) foxType = 0;
+            }
+
+            if (foxType == 0) {
+                currentImage = image;
+            }
+            if (foxType == 1) {
+                currentImage = image1;
+            }
+
+        }
+
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        public void mouseExited(MouseEvent e) {
+
+        }
+
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
 
     }
 }
+
